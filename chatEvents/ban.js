e0a6ad2943553b74eeae5cc855aa3e6ce73b7e82
zwertv_e <elliott.zwertvaegher@epitech.eu>
2017-10-18 17:16:45 +0200

    Ban now linked to API

--- chatEvents/ban.js
+++ chatEvents/ban.js
@@ -24,9 +24,34 @@ module.exports = {
                 });
                 if (clientToBan != null) {
                     console.log('Banning...' + client.username);
+                    var uidToBan = clientToBan.uid;
                     clientToBan.emit('ban', {'message': 'Disconnected', 'reason':'You have been banned !'});
                     client.emit('ban', {'message': 'Success', 'reason': data.username + ' has been banned'});
                     clientToBan.disconnect(true);
+
+                    var options = {
+                        host: config.API_URL,
+                        path: '/streams/' + clientToMute.room + '/chat/ban',
+                        method: 'POST',
+                        headers: {'Content-Type': 'application/json', 'Authorization': client.token}
+                    };
+
+                    var req = https.request(options, function(e) {
+                        var str = '';
+                        console.log('request ban to API');
+                        console.log('statusCode:', e.statusCode);
+                        console.log('headers:', e.headers);
+                        e.on('data', (chunck) => {console.log('Response: ' + chunck);str += chunck;});
+                        e.on('end', () => {console.log(str);});
+                        e.on('error', (err) => {console.log(err);});
+                    });
+				    var ban_data = {};
+				    ban_data['id'] = uidToBan;
+				    ban_data['duration'] = 500;
+				    var r_a = JSON.stringify(ban_data);
+                    console.log(r_a);
+				    req.write(r_a);
+				    req.end();
                 } else {
                     client.emit('cerror', {'code': 400, 'message': 'Ban error (user not found) !'});
                     console.log('Client ban error ! (user not found)');
